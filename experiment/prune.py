8641b5986fc062cb58a4f1de8ecbc46e345e4e1c
jonahobw <jgow98@gmail.com>
2021-10-31 02:39:03 -0400

    format and linting

--- experiment/prune.py
+++ experiment/prune.py
@@ -1,53 +1,103 @@
+"""Pruning a DNN."""
+
+# pylint: disable=relative-beyond-top-level, too-many-arguments, invalid-name
+# pylint: disable=unspecified-encoding, too-many-locals
+
 import json
-
+from typing import Callable
 from .train import TrainingExperiment
-
 from .. import strategies
 from ..metrics import model_size, flops
 from ..util import printc
 
 
 class PruningExperiment(TrainingExperiment):
+    """Prune a DNN."""
 
-    def __init__(self,
-                 dataset,
-                 model,
-                 strategy,
-                 compression,
-                 seed=42,
-                 path=None,
-                 dl_kwargs=dict(),
-                 train_kwargs=dict(),
-                 debug=False,
-                 pretrained=False,
-                 resume=None,
-                 resume_optim=False,
-                 save_freq=10,
-                 checkpoint_metric=None,
-                 early_stop=None,
-                 lr_schedule=None,
-                 gpu=None
-                 ):
+    def __init__(
+        self,
+        dataset: str,
+        model: str,
+        strategy: str,
+        compression: int,
+        seed: int = 42,
+        path: str = None,
+        dl_kwargs: {} = None,
+        train_kwargs: {} = None,
+        debug: bool = False,
+        pretrained: bool = False,
+        resume: str = None,
+        resume_optim: bool = False,
+        save_freq: int = 10,
+        checkpoint_metric: str = None,
+        early_stop: str = None,
+        lr_schedule: Callable = None,
+        gpu: int = None,
+    ):
+        """
+        Setup class variabls.
 
-        super(PruningExperiment, self).__init__(dataset, model, seed, path, dl_kwargs, train_kwargs, debug, pretrained, resume, resume_optim, save_freq, checkpoint_metric, early_stop, lr_schedule, gpu)
+        :param dataset: dataset to train on.
+        :param model: the architecture of the model.
+        :param strategy: the pruning strategy.
+        :param compression: the desired ratio of parameters in original to pruned model.
+        :param seed: the random seed for uid generation.
+        :param path: the path to the folder where results will be stored.
+        :param dl_kwargs: args for the dataloader.
+        :param train_kwargs: args for training, including number of epochs, optimizer type,
+            learning rate, and weight decay.
+        :param debug: whether or not to run in debug mode.
+        :param pretrained: whether or not to use a pretrained model.
+        :param resume: path to a pretrained model.
+        :param resume_optim: whether or not to resume from a previous optimizer.
+        :param save_freq: how many epochs to wait between saving models (best model seen so far
+            will still be saved regardless of this parameter).
+        :param checkpoint_metric: metric to use to figure out which is the best model seen
+            during training.
+        :param early_stop: method for determining early training termination.
+        :param lr_schedule: function with signature (epoch) which returns the learning rate
+            for that epoch.
+        :param gpu: the number of the gpu to run on.
+        """
+
+        super().__init__(
+            dataset,
+            model,
+            seed,
+            path,
+            dl_kwargs,
+            train_kwargs,
+            debug,
+            pretrained,
+            resume,
+            resume_optim,
+            save_freq,
+            checkpoint_metric,
+            early_stop,
+            lr_schedule,
+            gpu,
+        )
         self.add_params(strategy=strategy, compression=compression)
 
         self.apply_pruning(strategy, compression)
 
         self.path = path
         self.save_freq = save_freq
+        self.debug = debug
+        self.metrics = None
 
-    def apply_pruning(self, strategy, compression):
-        printc(f"applying {strategy} to {self.model}", color='GREEN')
+    def apply_pruning(self, strategy: str, compression: int) -> None:
+        """Apply the pruning to the model."""
         constructor = getattr(strategies, strategy)
         x, y = next(iter(self.train_dl))
         self.pruning = constructor(self.model, x, y, compression=compression)
         self.pruning.apply()
-        printc("Masked model", color='GREEN')
+        printc("Masked model", color="GREEN")
 
-    def run(self):
+    def run(self) -> None:
+        """Run the finetuning and metrics on the model."""
         self.freeze()
-        printc(f"Running {repr(self)}", color='YELLOW')
+        printc(f"Running {repr(self)}", color="YELLOW")
         self.to_device()
         self.build_logging(self.train_metrics, self.path)
 
@@ -56,40 +106,41 @@ class PruningExperiment(TrainingExperiment):
         if self.pruning.compression > 1:
             self.run_epochs()
 
-    def save_metrics(self):
+    def save_metrics(self) -> None:
+        """Save the pruning stats."""
         self.metrics = self.pruning_metrics()
-        with open(self.path / 'metrics.json', 'w') as f:
+        with open(self.path / "metrics.json", "w") as f:
             json.dump(self.metrics, f, indent=4)
-        printc(json.dumps(self.metrics, indent=4), color='GRASS')
+        printc(json.dumps(self.metrics, indent=4), color="GRASS")
         summary = self.pruning.summary()
-        summary_path = self.path / 'masks_summary.csv'
+        summary_path = self.path / "masks_summary.csv"
         summary.to_csv(summary_path)
         print(summary)
 
-    def pruning_metrics(self):
-
+    def pruning_metrics(self) -> {}:
+        """Collect the pruning metrics."""
         metrics = {}
         # Model Size
         size, size_nz = model_size(self.model)
-        metrics['size'] = size
-        metrics['size_nz'] = size_nz
-        metrics['compression_ratio'] = size / size_nz
+        metrics["size"] = size
+        metrics["size_nz"] = size_nz
+        metrics["compression_ratio"] = size / size_nz
 
         x, y = next(iter(self.val_dl))
         x, y = x.to(self.device), y.to(self.device)
 
         # FLOPS
         ops, ops_nz = flops(self.model, x)
-        metrics['flops'] = ops
-        metrics['flops_nz'] = ops_nz
-        metrics['theoretical_speedup'] = ops / ops_nz
+        metrics["flops"] = ops
+        metrics["flops_nz"] = ops_nz
+        metrics["theoretical_speedup"] = ops / ops_nz
 
         # Accuracy
         loss, acc1, acc5 = self.run_epoch(False, -1)
         self.log_epoch(-1)
 
-        metrics['loss'] = loss
-        metrics['val_acc1'] = acc1
-        metrics['val_acc5'] = acc5
+        metrics["loss"] = loss
+        metrics["val_acc1"] = acc1
+        metrics["val_acc5"] = acc5
 
         return metrics
--- experiment/train.py
+++ experiment/train.py
@@ -1,5 +1,13 @@
+"""Training a DNN."""
+
+# pylint: disable=relative-beyond-top-level, too-many-instance-attributes
+# pylint: disable=invalid-name, too-many-arguments, too-many-locals
+# pylint: disable=no-member
+
 import pathlib
 import time
+from typing import Callable, Union
+import json
 
 import torch
 import torchvision.models
@@ -7,7 +15,6 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.backends import cudnn
 from tqdm import tqdm
-import json
 
 from .base import Experiment
 from .. import datasets
@@ -18,6 +25,7 @@ from ..util import printc, OnlineStats
 
 
 class TrainingExperiment(Experiment):
+    """Training a DNN."""
 
     default_dl_kwargs = {"batch_size": 128, "pin_memory": False, "num_workers": 8}
 
@@ -29,61 +37,69 @@ class TrainingExperiment(Experiment):
 
     def __init__(
         self,
-        dataset,
-        model,
-        seed=42,
-        path=None,
-        dl_kwargs=dict(),
-        train_kwargs=dict(),
-        debug=False,
-        pretrained=False,
-        resume=None,
-        resume_optim=False,
-        save_freq=10,
-        checkpoint_metric=None,
-        early_stop = None,
-        lr_schedule = None,
-        gpu = None
-    ):
+        dataset: str,
+        model: str,
+        seed: int = 42,
+        path: str = None,
+        dl_kwargs: {} = None,
+        train_kwargs: {} = None,
+        debug: bool = False,
+        pretrained: bool = False,
+        resume: str = None,
+        resume_optim: bool = False,
+        save_freq: int = 10,
+        checkpoint_metric: str = None,
+        early_stop: str = None,
+        lr_schedule: Callable = None,
+        gpu: int = None,
+    ) -> None:
 
         """
+        Setup class variables.
 
-        :param dataset:
-        :param model:
-        :param seed:
-        :param path:
-        :param dl_kwargs:
-        :param train_kwargs:
-        :param debug:
-        :param pretrained:
-        :param resume:
-        :param resume_optim:
-        :param save_freq:
-        :param checkpoint_metric: either 'top1_acc', 'top5_acc', or 'train_loss' and then during training the model
-            with the best metric is saved
-        :param early_stop: gives a method to implement early stopping,
-        :param gpu: integer of gpu to run on
+        :param dataset: dataset to train on.
+        :param model: the architecture of the model.
+        :param seed: the random seed for uid generation.
+        :param path: the path to the folder where results will be stored.
+        :param dl_kwargs: args for the dataloader.
+        :param train_kwargs: args for training, including number of epochs, optimizer type,
+            learning rate, and weight decay.
+        :param debug: whether or not to run in debug mode.
+        :param pretrained: whether or not to use a pretrained model.
+        :param resume: path to a pretrained model.
+        :param resume_optim: whether or not to resume from a previous optimizer.
+        :param save_freq: how many epochs to wait between saving models (best model seen so far
+            will still be saved regardless of this parameter).
+        :param checkpoint_metric: metric to use to figure out which is the best model seen
+            during training.
+        :param early_stop: method for determining early training termination.
+        :param lr_schedule: function with signature (epoch) which returns the learning rate
+            for that epoch.
+        :param gpu: the number of the gpu to run on.
         """
 
         # Default children kwargs
-        super(TrainingExperiment, self).__init__(seed)
+        super().__init__(seed)
         dl_kwargs = {**self.default_dl_kwargs, **dl_kwargs}
         train_kwargs = {**self.default_train_kwargs, **train_kwargs}
 
         self.resume = resume
         self.path = path
         self.save_freq = save_freq
+        self.debug = debug
 
         # the metric that will be used to find the best model
         self.checkpoint_metric = checkpoint_metric
         # value of the best model metric
         self.best_metrics = None
         self.gpu = gpu
+        self.device = None  # gets set when calling self.run()
+        self.early_stop = early_stop
 
         params = locals()
         params["dl_kwargs"] = dl_kwargs
         params["train_kwargs"] = train_kwargs
-        params.pop('lr_schedule')
+        params.pop("lr_schedule")
         if path is not None:
             params["path"] = str(path)
         self.add_params(**params)
@@ -93,11 +109,16 @@ class TrainingExperiment(Experiment):
 
         self.build_model(model, pretrained, resume)
 
-        self.build_train(resume_optim=resume_optim, **train_kwargs)
+        self.build_train(
+            resume_optim=resume_optim, lr_schedule=lr_schedule, **train_kwargs
+        )
 
-    def check_best_metric(self, metrics):
+    def check_best_metric(self, metrics: {}) -> bool:
         """
         Returns true if a model metric is the best seen so far.
+        Uses self.checkpoint_metric for comparison.
+
+        :param metrics: dictionary of model metrics from the last epoch.
 
         :return: True if metric is the best seen so far, else False
         """
@@ -108,30 +129,50 @@ class TrainingExperiment(Experiment):
             return True
 
         # compare the new metric value with the best seen and update state if new metric is better
-        if 'loss' in self.checkpoint_metric and metrics[self.checkpoint_metric] < self.best_metrics[self.checkpoint_metric]:
+        if (
+            "loss" in self.checkpoint_metric
+            and metrics[self.checkpoint_metric]
+            < self.best_metrics[self.checkpoint_metric]
+        ):
             self.best_metrics = metrics
             return True
-        if 'acc' in self.checkpoint_metric and metrics[self.checkpoint_metric] > self.best_metrics[
-            self.checkpoint_metric]:
+        if (
+            "acc" in self.checkpoint_metric
+            and metrics[self.checkpoint_metric]
+            > self.best_metrics[self.checkpoint_metric]
+        ):
+            self.best_metrics = metrics
             return True
 
         return False
 
-    def run(self):
+    def run(self) -> None:
+        """Run the training."""
+
         self.freeze()
         printc(f"Running {repr(self)}", color="YELLOW")
         self.to_device()
         self.build_logging(self.train_metrics, self.path)
+        time.sleep(1)
         self.run_epochs()
 
-    def build_dataloader(self, dataset, **dl_kwargs):
+    def build_dataloader(self, dataset: str, **dl_kwargs) -> None:
+        """Build the dataloader."""
+
         constructor = getattr(datasets, dataset)
         self.train_dataset = constructor(train=True)
         self.val_dataset = constructor(train=False)
         self.train_dl = DataLoader(self.train_dataset, shuffle=True, **dl_kwargs)
         self.val_dl = DataLoader(self.val_dataset, shuffle=False, **dl_kwargs)
 
-    def build_model(self, model, pretrained=True, resume=None):
+    def build_model(
+        self,
+        model: Union[str, torchvision.models],
+        pretrained: bool = True,
+        resume: str = None,
+    ) -> None:
+        """Build the model."""
+
         if isinstance(model, str):
             if hasattr(models, model):
                 model = getattr(models, model)(pretrained=pretrained)
@@ -153,7 +194,16 @@ class TrainingExperiment(Experiment):
             previous = torch.load(self.resume)
             self.model.load_state_dict(previous["model_state_dict"])
 
-    def build_train(self, optim, epochs, resume_optim=False, **optim_kwargs):
+    def build_train(
+        self,
+        optim: str,
+        epochs: int,
+        lr_schedule: Callable,
+        resume_optim: bool = False,
+        **optim_kwargs,
+    ) -> None:
+        """Generate optimizer and learning rate scheduler."""
+
         default_optim_kwargs = {
             "SGD": {"momentum": 0.9, "nesterov": True, "lr": 1e-3},
             "Adam": {"momentum": 0.9, "betas": (0.9, 0.99), "lr": 1e-4},
@@ -178,7 +228,12 @@ class TrainingExperiment(Experiment):
         # Assume classification experiment
         self.loss_func = nn.CrossEntropyLoss()
 
-    def to_device(self):
+        # set up the optimizer to adjust learning rate during training
+        self.lr_scheduler = torch.optim.lr_scheduler.LambdaLR(self.optim, lr_schedule)
+
+    def to_device(self) -> None:
+        """Move model to CPU/GPU."""
+
         # Torch CUDA config
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if self.gpu is not None:
@@ -191,7 +246,9 @@ class TrainingExperiment(Experiment):
         self.model.to(self.device)
         cudnn.benchmark = True  # For fast training.
 
-    def checkpoint(self):
+    def checkpoint(self) -> None:
+        """Save model parameters from last epoch."""
+
         checkpoint_path = self.path / "checkpoints"
         checkpoint_path.mkdir(exist_ok=True, parents=True)
         epoch = self.log_epoch_n
@@ -203,42 +260,55 @@ class TrainingExperiment(Experiment):
             checkpoint_path / f"checkpoint-{epoch}.pt",
         )
 
-    def run_epochs(self):
+    def run_epochs(self) -> None:
+        """Run the training epochs."""
 
         since = time.time()
         try:
-            # optionally set up the optimizer to adjust learning rate during training
-            if hasattr(self, 'lr_schedule'):
-                scheduler = torch.optim.lr_scheduler.LambdaLR(self.optim, self.lr_schedule, verbose=True)
             for epoch in range(self.epochs):
-                printc(f"Start epoch {epoch}", color="YELLOW")
                 loss, acc1, acc5 = self.train(epoch)
                 val_loss, val_acc1, val_acc5 = self.eval(epoch)
 
-                metrics = {'train_loss': loss, 'train_acc1': acc1, 'train_acc5':acc5, 'val_loss': val_loss, 'val_acc1': val_acc1, 'val_acc5': val_acc5}
-                # Checkpoint epochs if it is the best model seen so far or if save frequency is triggered
-                if (
-                    hasattr(self, "checkpoint_metric")
-                    and self.check_best_metric(metrics)
+                metrics = {
+                    "train_loss": loss,
+                    "train_acc1": acc1,
+                    "train_acc5": acc5,
+                    "val_loss": val_loss,
+                    "val_acc1": val_acc1,
+                    "val_acc5": val_acc5,
+                }
+                # Checkpoint epochs if it is the best model seen so far or if save frequency
+                # is triggered
+                if hasattr(self, "checkpoint_metric") and self.check_best_metric(
+                    metrics
                 ):
+                    print(
+                        f"{self.checkpoint_metric} ({metrics[self.checkpoint_metric]}) "
+                        f"improved over previous best, checkpointing model."
+                    )
                     self.checkpoint()
                 elif epoch % self.save_freq == 0:
+                    print("Save frequency met, checkpointing model")
                     self.checkpoint()
-                # optionally update the learning rate
-                if hasattr(self, 'lr_schedule'):
-                    scheduler.step()
 
                 self.log(timestamp=time.time() - since)
                 self.log_epoch(epoch)
 
+                # update the learning rate (must be done once per epoch, or once per
+                #   training + testing iteration, that is why this is done in this
+                #   function rather than self.run_epoch())
+                self.lr_scheduler.step()
+
                 # optionally stop early
-                if hasattr(self, 'early_stop') and self.stop_early(metrics):
+                if hasattr(self, "early_stop") and self.early_stop(metrics):
                     break
 
         except KeyboardInterrupt:
             printc(f"\nInterrupted at epoch {epoch}. Tearing Down", color="RED")
 
-    def run_epoch(self, train, epoch=0):
+    def run_epoch(self, train: bool, epoch: int = 0) -> tuple(int, int, int):
+        """Run a single epoch."""
+
         if train:
             self.model.train()
             prefix = "train"
@@ -251,12 +321,16 @@ class TrainingExperiment(Experiment):
         total_loss = OnlineStats()
         acc1 = OnlineStats()
         acc5 = OnlineStats()
+        step_size = OnlineStats()
+        step_size.add(self.lr_scheduler.get_last_lr()[0])
 
         epoch_iter = tqdm(dl)
         epoch_iter.set_description(f"{prefix.capitalize()} Epoch {epoch}/{self.epochs}")
 
         with torch.set_grad_enabled(train):
             for i, (x, y) in enumerate(epoch_iter, start=1):
+                if self.debug and i > 1:
+                    break
                 x, y = x.to(self.device), y.to(self.device)
                 yhat = self.model(x)
                 loss = self.loss_func(yhat, y)
@@ -272,7 +346,10 @@ class TrainingExperiment(Experiment):
                 acc5.add(c5 / dl.batch_size)
 
                 epoch_iter.set_postfix(
-                    loss=total_loss.mean, top1=acc1.mean, top5=acc5.mean
+                    loss=total_loss.mean,
+                    top1=acc1.mean,
+                    top5=acc5.mean,
+                    step_size=step_size.mean,
                 )
 
         self.log(
@@ -280,19 +357,23 @@ class TrainingExperiment(Experiment):
                 f"{prefix}_loss": total_loss.mean,
                 f"{prefix}_acc1": acc1.mean,
                 f"{prefix}_acc5": acc5.mean,
+                "lr": self.lr_scheduler.get_last_lr()[0],
             }
         )
 
         return total_loss.mean, acc1.mean, acc5.mean
 
-    def train(self, epoch=0):
+    def train(self, epoch: int = 0) -> tuple(int, int, int):
+        """Run a single training epoch."""
         return self.run_epoch(True, epoch)
 
-    def eval(self, epoch=0):
+    def eval(self, epoch: int = 0) -> tuple(int, int, int):
+        """Run a single validation epoch."""
         return self.run_epoch(False, epoch)
 
     @property
-    def train_metrics(self):
+    def train_metrics(self) -> list:
+        """Generate the training metrics to be logged to a csv."""
         return [
             "epoch",
             "timestamp",
@@ -302,9 +383,11 @@ class TrainingExperiment(Experiment):
             "val_loss",
             "val_acc1",
             "val_acc5",
+            "lr",
         ]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Dunders."""
         if not isinstance(self.params["model"], str) and isinstance(
             self.params["model"], torch.nn.Module
         ):
